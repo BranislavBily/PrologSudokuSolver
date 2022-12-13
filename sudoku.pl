a0c61c7ff2d45c5f65d9fa4130f1f470e8164d46
Branislav Bily <bilybran@fit.cvut.cz>
2022-12-13 10:11:30 +0100

    BROKEN: Nic nejde

--- sudoku.pl
+++ sudoku.pl
@@ -1,46 +1,42 @@
 :- use_module(library(lists)).
-%Usage of this is forbidden yey
-:- use_module(library(clpfd)).
+%Returns true if two "elements" are same
+%same/2(+X, +Y)
+same(X, Y) :- X = Y.
 
-sudoku(Puzzle) :-
-    %Check if the Puzzle is square
-    length(Puzzle, Height),
-    maplist(same_length(Puzzle), Puzzle),
-    first_row_size(Puzzle, Height),
+%If P is success, fail and never come back
+%notProvable/1(+P)
+notProvable(P) :- P, !, fail.
+notProvable(_).
 
-    %Check if all numbers are in range of 1 to Height
-    append(Puzzle, Numbers),
-    Numbers ins 1..Height,
+%Returns true if two "elements" are not the same
+%Yes I could have used \= but I wanted to try not predicate
+%not_same/2(+X, +Y)
+not_same(X, Y) :- notProvable(same(X, Y)).
 
-    %Now check all rows and columns is distinct
-    Rows = Puzzle,
-    maplist(all_distinct, Rows),
-    transpose(Rows, Columns),
-    maplist(all_distinct, Columns),
-
-    %Now check if all numbers in every square is distinct
-    check_squares(Rows).
+%Checks if all numbers in list are different by
+%checking every possible combination
+%all_numbers_different/1(+Lst)
+all_numbers_different([]).
+all_numbers_different([H|T]) :-
+    maplist(not_same(H), T),
+    all_numbers_different(T).
 
 %Checks if size of Row is the same as the Height of the sudoku
 %first_row_size/2(+Puzzle, +Height)
 first_row_size([Row|_], Height) :-
     length(Row, Height).
 
-%Checks all squares in Sudoku, if every square consists of only unique numbers
-%check_squares/1(+Rows)
-check_squares(Rows) :-
-    Rows = [A, B, C, D, E, F, G, H, I],
-    squares(A, B, C),
-    squares(D, E, F),
-    squares(G, H, I).
+check_sudoku_contraints(Puzzle) :-
+    %Check if the Puzzle is square
+    length(Puzzle, Height),
+    maplist(same_length(Puzzle), Puzzle),
+    first_row_size(Puzzle, Height),
 
-%Creates square from each row and checks if all numbers in them are distinct
-%squares/3(+Row1, +Row2, Row3)
-squares([], [], []).
-squares([N1, N2, N3| Row1],
-        [N4, N5, N6| Row2],
-        [N7, N8, N9| Row3]) :-
-            all_distinct([N1, N2, N3, N4, N5, N6, N7, N8, N9]),
-            squares(Row1, Row2, Row3).
-    
-    
+    append(Puzzle, Numbers),
+    %Check if all numbers are in range of 1 to Height
+    maplist(between(1, Height), Numbers),
+    %Check if all numbers are in range of 1 to Height
+    maplist(all_numbers_different, Puzzle).
+
+sudoku(Puzzle) :-
+    check_sudoku_contraints(Puzzle).
---
+++ sudoku.plt
@@ -0,0 +1,5 @@
+:- begin_tests(sudoku).
+
+test(same) :- same(1,1).
+
+:- end_tests(sudoku).
